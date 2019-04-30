2809d6878e1f80f3cdec81afad8bf64075dc78f8
Dikshyant Rai <drai@Dikshyants-MBP.fios-router.home>
2019-04-30 19:48:13 -0400

    first commit

---
+++ .eslintrc.js
@@ -0,0 +1,17 @@
+module.exports = {
+  extends: 'mourner',
+  parserOptions: {
+    sourceType: 'module'
+  },
+  rules: {
+    strict: [0],
+    camelcase: [0],
+    'no-loop-func': [0],
+    'object-curly-spacing': [0],
+    'consistent-return': [0],
+    'valid-jsdoc': [2, {
+      prefer: {'return': 'returns'},
+      requireReturn: false
+    }]
+  }
+};
---
+++ src/bench.js
@@ -0,0 +1,29 @@
+const fs = require('fs');
+const path = require('path');
+const load = require('load-json-file');
+const Benchmark = require('benchmark');
+const union = require('./').default;
+
+const directories = {
+    in: path.join(__dirname, 'test', 'in') + path.sep,
+    out: path.join(__dirname, 'test', 'out') + path.sep
+};
+
+const fixtures = fs.readdirSync(directories.in).map(filename => {
+    return {
+        filename,
+        name: path.parse(filename).name,
+        geojson: load.sync(directories.in + filename)
+    };
+});
+
+const suite = new Benchmark.Suite('turf-union');
+
+for (const {name, geojson} of fixtures) {
+    suite.add(name, () => { union.apply(this, geojson.features); });
+}
+
+suite
+  .on('cycle', e => console.log(String(e.target)))
+  .on('complete', () => {})
+  .run();
